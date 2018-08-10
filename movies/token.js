d06bb153734d4a5c5c2173d8056ff8d5441e2830
Abraham Walters <abraham.walters@capitalone.com>
2018-08-10 14:12:08 -0400

    Updated token endpoint to make dynamodb call and user check

--- movies/token.js
+++ movies/token.js
@@ -1,5 +1,8 @@
 const Joi = require('joi');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const ddb = require('serverless-dynamodb-client');
+const dynamoDb = ddb.doc;
 
 const config = require('./config');
 const schema = Joi.object().keys(config.schemas.user);
@@ -17,18 +20,49 @@ module.exports.token = (event, context, callback) => {
     });
   }
 
-  const user = {};
-
-  const token = jwt.sign(
-    { user },
-    process.env.JWT_SECRET,
-    { expiresIn: config.auth.jwt.expiresIn }
-  );
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(token)
+  const params = {
+    TableName: process.env.DYNAMODB_USER_TABLE,
+    Key: {
+      username: decodeURIComponent(event.pathParameters.username)
+    },
   };
 
-  callback(null, response);
+  // fetch todo from the database
+  dynamoDb.get(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error('~~~', error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch user.',
+      });
+      return;
+    }
+
+    console.log('~~~ result', result);
+    const user = result.Item;
+    let response;
+
+    if (bcrypt.compareSync(data.password, user.password)) {
+      const token = jwt.sign(
+        { user },
+        process.env.JWT_SECRET,
+        { expiresIn: config.auth.jwt.expiresIn }
+      );
+
+      response = {
+        statusCode: 200,
+        body: JSON.stringify(token)
+      };
+    } else {
+      response = {
+        statusCode: 400,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Unable to serve token - incorrect user password.',
+      };
+    }
+
+    callback(null, response);
+  });
 };
