516d32d0d58bc8798fb8b59d0f518bedffc547a2
Abraham Walters <abraham.walters@capitalone.com>
2018-08-10 21:02:42 -0400

    Updated load-movies script to disregard https while loading movies.json

--- load-movies.js
+++ load-movies.js
@@ -1,17 +1,22 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
+const https = require('https');
 
 const main = () => {
-  const file = fs.readFileSync('movies.json', 'utf8')
+  const agent = new https.Agent({
+    rejectUnauthorized: false
+  });
+  const file = fs.readFileSync('movies.json', 'utf8');
   const { movies } = JSON.parse(file);
 
   movies.forEach(async (movie) => {
     const opts = {
       method: 'POST',
-      body: JSON.stringify(movie)
+      body: JSON.stringify(movie),
+      agent
     };
     
-    await fetch('http://localhost:3000/movies', opts);
+    await fetch('https://localhost:3000/movies', opts);
   });
 };
 
