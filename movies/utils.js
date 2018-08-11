83fff85575cd39e3f55aa101527d3692ad3fae61
Abraham Walters <abraham.walters@capitalone.com>
2018-08-11 11:23:13 -0400

    Added additional utils for requesting data

--- movies/utils.js
+++ movies/utils.js
@@ -1,3 +1,8 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const https = require('https');
+const humps = require('humps');
+
 /**
   * Returns an IAM policy document for a given user and resource.
   * @param {String} principalId User Id
@@ -6,7 +11,7 @@
   * @param {String} context     Response context
   * @returns {Object}           PolicyDocument
   */
- module.exports.getIAMPolicy = (principalId, Effect, Resource, context) => {
+const getIAMPolicy = (principalId, Effect, Resource, context) => {
   return {
     principalId,
     policyDocument: {
@@ -22,6 +27,37 @@
     context
   };
 };
+module.exports.getIAMPolicy = getIAMPolicy;
+
+/**
+ * Adds a timestamp to the end of a url to prevent caching
+ * @param {string} url  Any URL
+ * @return {string}     URL + timestamp
+ */
+const getNoCacheUrl = (url) => {
+  const now = Date.now();
+  return url.includes('?') ? `${url}&${now}` : `${url}?${now}`;
+};
+module.exports.getNoCacheUrl = getNoCacheUrl;
+
+/**
+ * Adds the HTTPS agent to the fetch request options
+ * @param {Object} opts Fetch request options
+ * @return {Object}     HTTPS fetch request options
+ */
+const getHTTPSOpts = (opts) => {
+  return merge(opts, { agent: HTTPSAgent });
+};
+module.exports.getHTTPSOpts = getHTTPSOpts;
+
+/**
+ * @type {Object}
+ * HTTPS agent for secure localhost requests
+ */
+const HTTPSAgent = new https.Agent({
+  ca: fs.readFileSync('auth/cert.pem'), 
+});
+module.exports.HTTPSAgent = HTTPSAgent;
 
 /**
  * Is the user authorized to acess the AWS resource?
@@ -29,7 +65,7 @@
  * @param {string} methodArn      Lambda AWS ARN
  * @return {boolean}              True, if user is authorized; false, otherwise
  */
-module.exports.isAuthorized = (scopes, methodArn) => {
+const isAuthorized = (scopes, methodArn) => {
   if (scopes.includes('*')) {
     return true;
   }
@@ -42,3 +78,42 @@ module.exports.isAuthorized = (scopes, methodArn) => {
 
   return false;
 };
+module.exports.isAuthorized = isAuthorized;
+
+/**
+ * Merges any number of JavaScript objects into a single object.
+ * Objects are folded-right, so that the right most object is
+ * given priority.
+ * @param {Objects} ...objs Any number of JavaScript objects
+ * @return {Object}         Merged objects
+ */
+const merge = (...objs) => {
+  return objs.reduce((merged, obj) => {
+    return Object.assign({}, merged, obj);
+  });
+};
+
+/**
+ * Wraps a request with proper no caching,
+ * HTTPS cert, & error handling, then returns the
+ * json response with camel-cased keys.
+ * @param {string} url  URL
+ * @param {Object} opts Fetch options
+ * @return {*}          Response
+ */
+const req = async (url, opts) => {
+  const noCacheUrl = getNoCacheUrl(url);
+  const httpsOpts = url.includes('https') ? getHTTPSOpts(opts) : opts;
+  const res = await fetch(noCacheUrl, httpsOpts);
+
+  if (res.status < 600 && res.status >= 400) {
+    const err = await res.json();
+    console.error('~~~ Request Error', JSON.stringify(err, null, 2));
+    throw new Error(err);
+  }
+
+  const json = await res.json();
+
+  return humps.camelizeKeys(json);
+};
+module.exports.req = req;
