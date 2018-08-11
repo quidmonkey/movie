7f127e6f3257eefd0be1aace859836d9ee9b90ab
Abraham Walters <abraham.walters@capitalone.com>
2018-08-11 09:54:42 -0400

    Added token fetching to load-movies script.
    Fixed issue where user endpoint was requiring auth.

--- load-movies.js
+++ load-movies.js
@@ -2,18 +2,35 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const https = require('https');
 
-const loadMovies = () => {
+const loadMovies = async () => {
   const agent = new https.Agent({
     ca: fs.readFileSync('auth/cert.pem'), 
   });
   const file = fs.readFileSync('movies.json', 'utf8');
   const { movies } = JSON.parse(file);
+  const user = {
+    username: Date.now().toString(),
+    password: Date.now().toString()
+  };
+  const opts = {
+    method: 'POST',
+    body: JSON.stringify(user),
+    agent
+  };
+
+  await fetch('https://localhost:3000/movies/user', opts);
+
+  const tokenRes = await fetch('https://localhost:3000/movies/token', opts);
+  const { token } = await tokenRes.json();
 
   movies.forEach(async (movie) => {
     const opts = {
       method: 'POST',
       body: JSON.stringify(movie),
-      agent
+      agent,
+      headers: {
+        Authorization: token
+      }
     };
     
     await fetch('https://localhost:3000/movies', opts);
