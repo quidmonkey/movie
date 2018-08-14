d45bef3e1fb9d9bf05748d89dea891653d251be1
Abraham Walters <abraham.walters@capitalone.com>
2018-08-14 15:03:26 -0400

    Moved back from .secret to secrets.json file.
    Added rule to have serverless read secrets.json file first if it exists, and then to pull from ssm. Re-added secrets.json to gitignore.

--- auth/deploy-secret.sh
+++ auth/deploy-secret.sh
@@ -2,7 +2,25 @@
 
 cwd=$(dirname $0)
 
-source $cwd/.secret
+##
+# Get the value of a given key in a json file
+#
+# Arguments:
+#   1. json filepath
+#   2. json key
+#
+# Returns:
+#   Value of given json key
+#
+##
+function get_json_value() {
+  local filepath=$1
+  local key=$2
+
+  grep -o "\"$key\": \"[^\"]*" $filepath | grep -o '[^"]*$'
+}
+
+JWT_SECRET=$(get_json_value $cwd/secrets.json "JWT_SECRET")
 
 [ -z "$JWT_SECRET" ] && echo "JWT_SECRET is not set" && exit 1
 
