e5eb0005216ae5b88addb3e1e8267d2c67de8568
Abraham Walters <abraham.walters@capitalone.com>
2018-08-14 15:42:30 -0400

    Updated create-secret and deploy-secret scripts to make them smarter.
    Both now log helpful output to stdout, and deploy-secret will check the current secret in ssm and only upload if need be.

--- auth/create-secret.sh
+++ auth/create-secret.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-cwd=$(dirname $0)
+cwd=$(dirname ${BASH_SOURCE[0]})
 
 if [ ! -f $cwd/secrets.json ]; then 
+  echo "~~~ Missing secrets.json. Generating file."
   uuid=$(uuidgen | sed 's/-//g')
   echo "{"  > $cwd/secrets.json
-  echo "  \"JWT_SECRET\":\"$uuid\"" >> $cwd/secrets.json
+  echo "  \"JWT_SECRET\": \"$uuid\"" >> $cwd/secrets.json
   echo "}" >> $cwd/secrets.json
+else
+  echo "~~~ secrets.json found. No need to generate."
 fi
--- auth/deploy-secret.sh
+++ auth/deploy-secret.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cwd=$(dirname $0)
+cwd=$(dirname ${BASH_SOURCE[0]})
 
 ##
 # Get the value of a given key in a json file
 #
 # Arguments:
-#   1. json filepath
+#   1. json src cmd (either filepath or string) i.e. "cat path/to/file.json" or "echo $json_blob"
 #   2. json key
 #
 # Returns:
@@ -14,14 +14,22 @@ cwd=$(dirname $0)
 #
 ##
 function get_json_value() {
-  local filepath=$1
+  local src=$1
   local key=$2
 
-  grep -o "\"$key\": \"[^\"]*" $filepath | grep -o '[^"]*$'
+  $src | grep -o "\"$key\": \"[^\"]*" | grep -o '[^"]*$'
 }
 
-JWT_SECRET=$(get_json_value $cwd/secrets.json "JWT_SECRET")
+JWT_SECRET=$(get_json_value "cat $cwd/secrets.json" "JWT_SECRET")
 
 [ -z "$JWT_SECRET" ] && echo "JWT_SECRET is not set" && exit 1
 
-aws ssm put-parameter --overwrite --name JWT_SECRET --type String --value $JWT_SECRET
+AWS_RES=$(aws ssm get-parameters --names "JWT_SECRET")
+AWS_JWT_SECRET=$(get_json_value "echo $AWS_RES" "Value")
+
+if [ -z "$AWS_JWT_SECRET" ] || [ "$JWT_SECRET" != "$AWS_JWT_SECRET" ]; then
+  echo "~~~ Deploying New Key to SSM"
+  aws ssm put-parameter --overwrite --name JWT_SECRET --type String --value $JWT_SECRET
+else
+  echo "~~~ JWT_SECRET up-to-date. No SSM deploy needed."
+fi
