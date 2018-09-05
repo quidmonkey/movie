5aa4f3cc0cdec4e6e9e9541fe7d51636947dad0e
Abraham Walters <abraham.walters@capitalone.com>
2018-09-05 10:47:57 -0400

    Added createMovie mutation

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,6 +1,31 @@
 const ddb = require('serverless-dynamodb-client');
 const dynamoDb = ddb.doc;
 
+const uuid = require('uuid');
+
+const createMovie = async({ movie }) => {
+  const { title, format, length, releaseYear, rating } = movie;
+  const timestamp = Date.now();
+  const params = {
+    TableName: process.env.DYNAMODB_MOVIES_TABLE,
+    Item: {
+      id: uuid.v1(),
+      createdAt: timestamp,
+      updatedAt: timestamp,
+
+      title,
+      format,
+      length,
+      releaseYear,
+      rating
+    }
+  };
+
+  await dynamoDb.put(params).promise();
+
+  return movie;
+};
+
 const getMovie = async ({ title }) => {
   const params = {
     TableName: process.env.DYNAMODB_MOVIES_TABLE,
@@ -24,6 +49,9 @@ const getMovies = async () => {
 };
 
 module.exports = {
+  Mutation: {
+    createMovie: async (root, args) => await createMovie(args)
+  },
   Query: {
     movie: async (root, args) => await getMovie(args),
     movies: async (root, args) => await getMovies()
--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -1,6 +1,14 @@
 const { gql } = require('apollo-server-lambda');
 
 module.exports = gql`
+  input MovieInput {
+    title: String
+    format: String
+    length: String
+    releaseYear: String
+    rating: Int
+  }
+
   type Movie {
     title: String!
     format: String!
@@ -9,9 +17,12 @@ module.exports = gql`
     rating: Int!
   }
 
+  type Mutation {
+    createMovie(movie: MovieInput!): Movie
+  }
+
   type Query {
-    movie(title: String!): Movie!
-    movies: [Movie],
-    user(username: String!): User!
+    movie(title: String!): Movie
+    movies: [Movie]
   }
 `;
