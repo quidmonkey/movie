a582a14a27d4d65f11e2d3a7d0a8f2c1052a8b4c
Abraham Walters <abraham.walters@capitalone.com>
2018-09-07 13:01:01 -0400

    Updated Joi config and typeDefs constraints to match each other

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const Joi = require('joi');
 
 const movie = {
-  title: Joi.string().min(1).max(50).required(),
-  format: Joi.string().valid('VHS', 'DVD', 'Streaming').required(),
-  length: Joi.string().regex(/[0-9]{1,3} min/).required(),
-  releaseYear: Joi.number().min(1800).max(2100).required(),
+  title: Joi.string().required(),
+  format: Joi.string().valid('Blu-Ray', 'DVD', 'Streaming').required(),
+  length: Joi.string().regex(/^[0-9]{1,3} min$/).required(),
+  releaseYear: Joi.number().min(1900).max(2100).required(),
   rating: Joi.number().min(1).max(5).required()
 };
 
