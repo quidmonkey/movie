eaeeb259eecdd5ede18304ed578c83fa6806583a
Abraham Walters <abraham.walters@capitalone.com>
2018-09-08 10:43:30 -0400

    Updated create.spec.js to create.test.js.
    Made all create endpoint tests now unit tests utilizing mock-dynamodb-utils

---
+++ src/create.test.js
@@ -0,0 +1,43 @@
+const {
+  getMockMovie,
+  mockMovieOne
+} = require('./mock-dynamodb-utils');
+
+const { create } = require('./create');
+
+it('create - should create a movie', async () => {
+  const event = {
+    body: JSON.stringify(mockMovieOne)
+  };
+  const res = await create(event);
+
+  expect(res.statusCode).toBe(200);
+
+  const expected = Object.keys(mockMovieOne).concat(['id', 'createdAt', 'updatedAt']).sort();
+  const actual = Object.keys(JSON.parse(res.body)).sort();
+
+  expect(actual).toEqual(expected);
+});
+
+it('create - should fail on an invalid request', async () => {
+  const movie = getMockMovie();
+  const event = {
+    body: JSON.stringify(movie)
+  };
+  const res = await create(event);
+
+  expect(res.statusCode).toBe(400);
+  expect(res.body).toBe('Validation Failed - Incorrect Movie Data Model.');
+});
+
+it('create - should fail on a DynamoDB error', async () => {
+  const movie = getMockMovie();
+  movie.title = 'error';
+  const event = {
+    body: JSON.stringify(movie)
+  };
+  const res = await create(event);
+
+  expect(res.statusCode).toBe(501);
+  expect(res.body).toBe('Couldn\'t create the movie.');
+});
--- src/mock-dynamodb-utils.js
+++ src/mock-dynamodb-utils.js
@@ -7,11 +7,11 @@ const faker = require('faker');
 
 process.env.DYNAMODB_MOVIES_TABLE = 'foobar';
 
-const MOCK_FORMAT_TYPES = ['BLURAY', 'DVD', 'STREAMING'];
-const getMockFormat = (index) => MOCK_FORMAT_TYPES[index];
+const GRAPHQL_FORMAT_TYPES = ['Blu-Ray', 'DVD', 'Streaming'];
+const getMockFormat = (index) => GRAPHQL_FORMAT_TYPES[index];
 const getMockMovie = () => {
   return {
-    format: getMockFormat(Math.round(Math.random() * MOCK_FORMAT_TYPES.length)),
+    format: getMockFormat(Math.round(Math.random() * GRAPHQL_FORMAT_TYPES.length)),
     length: `${Math.round(Math.random() * 300)} min`,
     releaseYear: 1900 + Math.round(Math.random() * 200),
     rating: 1 + Math.round(Math.random() * 4)
@@ -28,7 +28,16 @@ mockMovieTwo.title = faker.random.word();
 module.exports.mockMovieOne = mockMovieOne;
 module.exports.mockMovieTwo = mockMovieTwo;
 
-aws.mock('DynamoDB.DocumentClient', 'put', 'Success');
+const mockPutRes = 'Success';
+module.exports.mockPutRes = mockPutRes;
+
+aws.mock('DynamoDB.DocumentClient', 'put', async (params) => {
+  if (params.Item.title === 'error') {
+    throw new Error('Internal Server Error');
+  }
+
+  return mockPutRes;
+});
 aws.mock('DynamoDB.DocumentClient', 'get', async (params) => {
   return {
     Item: params.Key.title === mockMovieOne.title ? mockMovieOne : mockMovieTwo
